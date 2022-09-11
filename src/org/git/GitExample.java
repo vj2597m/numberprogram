13360ed7ca809291837af0af2527708d76513afb
vj2597m <vj052597@gmail.com>
2022-09-11 22:26:13 +0530

    vj@567

---
+++ src/org/git/GitExample.java
@@ -0,0 +1,27 @@
+package org.git;
+
+public class GitExample {
+	
+	public static void main(String[] args) {
+		int n1=0,n2=1,i,n3,count=100;
+		
+		System.out.println(n1+" "+n2);
+		
+		for (  i=2; i<count;i++) {
+			
+			n3=n1+n2;
+			System.out.print(" "+n3);
+			
+			n1=n2;
+			n2=n3;
+			
+			
+			
+		}
+		
+		}
+			
+		}
+		 
+		
+
